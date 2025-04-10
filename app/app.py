7a6d98a4ff0d018c99c77c36b26fe8f8b1258863
Mani Karthik Gurram <manikarthikgurram2001@gmail.com>
2025-04-10 20:54:29 +0530

    Included metrics in flask

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 @app.route('/')
 def hello():
     return "Hello from DevOps on Azure VM with CI/CD!"
