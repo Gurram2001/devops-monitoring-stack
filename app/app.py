64b3feb5a1b42acb06c35e2327888ab1a3b4a36d
Mani Karthik Gurram <101256564+Gurram2001@users.noreply.github.com>
2025-07-16 14:37:52 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 @app.route('/')
 def hello():
-    return "Hello from DevOps on Azure VM with CI/CD! - Done by manikarthik-gurram"
+    return "Hello from DevOps on Azure VM with CI/CD - Done by manikarthik-gurram"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
